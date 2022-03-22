428a24eef197e6d43de7dea17a04fc6e0071f9c3
Kreysh1 <44417554+Kreysh1@users.noreply.github.com>
2022-03-22 16:07:00 -0700

    TME-GEOD

--- src/app/components/calculator/calculator.component.ts
+++ src/app/components/calculator/calculator.component.ts
@@ -96,12 +96,12 @@ export class CalculatorComponent implements OnInit {
   } 
 
   //VARIABLES DE ENTRADA
-  Latitud:string = "23 15 34.7562";
-  Longitud:string = "111 12 32.6231";
+  Latitud:string = "25 41 20.1207898782595";
+  Longitud:string = "108 29 45.970220635063";
   Meridiano:string = "105 49 0";
   MeridianoFinal:string = "105 49 0";
-  Norte:number = 2572279.321;
-  Este:number = 478616.176;
+  Norte:number = 2848632.771;
+  Este:number = 500335.227;
   Zona:number = 12;
   ZonaFinal:number = 12;
 
@@ -117,8 +117,109 @@ export class CalculatorComponent implements OnInit {
   MeridianoM:number;
   MeridianoS:number;
 
-  TME2Geo(){
+  TME2Geo(Norte:number,Este:number,Meridiano:string){
+    //MERIDIANO CENTRAL
+    let z = Meridiano.split(" ", 3);
+
+    this.MeridianoG = Number(z[0]);                                                             //Meridiano Central Grados
+    this.MeridianoM = Number(z[1]);                                                             //Meridiano Minutos
+    this.MeridianoS = Number(z[2]);                                                             //Meridiano Segundos
+    let MeridianoDecimal:number = this.MeridianoG+(this.MeridianoM/60)+(this.MeridianoS/3600);  //Meridiano en Decimal
+
+    //DATUM
+    let a:number = 6378137;
+    let b:number = 6356752.3141;
+    let c:number = (Math.pow(a, 2)/b);
+    let e:number = ((Math.pow(a, 2)-Math.pow(b, 2))/Math.pow(a, 2));                    //e2
+    let ee:number = ((Math.pow(a, 2)-Math.pow(b, 2))/Math.pow(b, 2));;                  //e'2
+    let f:number = ((a-b)/a);                                                           //Achatamiento del Elipsoide de Referencia
+    let n:number = ((a-b)/(a+b));
+
+    //CALCULAR DISTANCIA MERIDIONAL (Se calcula en base al DATUM)
+    let A0:number = 0.994977106051944;
+    let A1:number = 0.005001851767455;
+    let A2:number = 0.004184862497658;
+    let A4:number = 0.000021791815429;
+    let A6:number = 0.000000123060249;
+    let A8:number = 0.000000000744632;
+
+    //CONSTANTES PARA CALCULO
+    let Ko:number = 1;                                                             //Factor de Escala
+    let No:number = 0;                                                                  //Falso Norte
+    let Eo:number = 500000;                                                             //Falso Este
+    let Lo:number = MeridianoDecimal;                                                   ////Longitud Meridiano Central (DECIMAL)
+
+    //VARIABLES DE LA INVERSA
+    let E:number = (Este-Eo)/Ko;
+    let N:number = (Norte-No)/Ko;
+
+    let Oo:number = (b)*((N-No)/(A0*Math.pow(a,2)));
+    let So:number = (A0*(Math.pow(a,2)/b)*Oo)-(((A1*(Math.pow(a,2)/b)*Math.sin(Oo)*Math.cos(Oo)))*(1+(A2*Math.pow(Math.sin(Oo),2))+(A4*Math.pow(Math.sin(Oo),4))+(A6*Math.pow(Math.sin(Oo),6))+(A8*Math.pow(Math.sin(Oo),8))));
+    let O1:number = Oo+(((b)*((N-No-So)))/(A0*Math.pow(a,2)));
+    let S1:number = (A0*(Math.pow(a,2)/b)*O1)-(((A1*(Math.pow(a,2)/b)*Math.sin(O1)*Math.cos(O1)))*(1+(A2*Math.pow(Math.sin(O1),2))+(A4*Math.pow(Math.sin(O1),4))+(A6*Math.pow(Math.sin(O1),6))+(A8*Math.pow(Math.sin(O1),8))));
+    let convergencia1:number = N-S1;
+    let O2:number = O1+(((b)*((N-No-S1)))/(A0*Math.pow(a,2)));
+    let S2:number = (A0*(Math.pow(a,2)/b)*O2)-(((A1*(Math.pow(a,2)/b)*Math.sin(O2)*Math.cos(O2)))*(1+(A2*Math.pow(Math.sin(O2),2))+(A4*Math.pow(Math.sin(O2),4))+(A6*Math.pow(Math.sin(O2),6))+(A8*Math.pow(Math.sin(O2),8))));
+    let convergencia2:number = N-S2;
+    let O3:number = O2+(((b)*((N-No-S2)))/(A0*Math.pow(a,2)));
+    let S3:number = (A0*(Math.pow(a,2)/b)*O3)-(((A1*(Math.pow(a,2)/b)*Math.sin(O3)*Math.cos(O3)))*(1+(A2*Math.pow(Math.sin(O3),2))+(A4*Math.pow(Math.sin(O3),4))+(A6*Math.pow(Math.sin(O3),6))+(A8*Math.pow(Math.sin(O3),8))));
+    let convergencia3:number = N-S3;
+
+    let Ob:number = O3;
+    let t2b:number = Math.pow((Math.tan(Ob)),2);
+    let n2b:number = e*Math.pow((Math.cos(Ob)),2);
+    let Nb:number = (a)/(Math.sqrt((1-(e*Math.pow((Math.sin(Ob)),2)))));
+    let Mb:number = (a*(1-e))/(Math.pow((1-(e*Math.pow((Math.sin(Ob)),2))),3/2));
+    let tb:number = Math.sqrt(t2b);
+
+    let Latitud:number[] = [];
+    let Longitud:number[] = [];
+
+    Latitud[0] = Ob;
+    Latitud[1] = (Math.pow(E,2)/(Mb*Nb))*(tb/2);
+    Latitud[2] = (Math.pow(E,4)/(Mb*Math.pow(Nb,3)))*(tb/24)*(5+(3*t2b)+n2b-(4*Math.pow(n2b,4))-(9*t2b*n2b));
+    Latitud[3] = (Math.pow(E,6)/(Mb*Math.pow(Nb,5)))*(tb/720)*(61+(90*t2b)+(46*n2b)+(45*Math.pow(n2b,2))-(252*t2b*n2b)-(3*Math.pow(n2b,2))+(100*Math.pow(n2b,3))-(66*t2b*Math.pow(n2b,2))-(90*n2b*Math.pow(t2b,2))+(88*Math.pow(n2b,4))+(225*Math.pow(t2b,2)*Math.pow(n2b,2))+(84*t2b*Math.pow(n2b,3))-(192*t2b*Math.pow(n2b,4)));
+    Latitud[4] = (Math.pow(E,8)/(Mb*Math.pow(Nb,7)))*(tb/40320)*(1385+(3633*t2b)+(4095*Math.pow(t2b,2))+(1575*Math.pow(t2b,3)));
+    Latitud[5] = Latitud[0]-Latitud[1]+Latitud[2]-Latitud[3]+Latitud[4];
+    Latitud[6] = Latitud[5]/(Math.PI/180);
     
+    Longitud[0] = (E)/(Math.cos(Ob)*Nb);
+    Longitud[1] = (1/(6*Math.cos(Ob)))*(Math.pow((E/Nb),3))*((1+(2*t2b)+n2b));
+    Longitud[2] = (1/(120*Math.cos(Ob)))*(Math.pow((E/Nb),5))*(5+(6*n2b)+(28*t2b)-(3*Math.pow(n2b,2))+(8*t2b*n2b)+(24*Math.pow(t2b,2))-(4*Math.pow(n2b,3))+(4*t2b*Math.pow(n2b,2))+(24*t2b*Math.pow(n2b,3)));
+    Longitud[3] = (1/(5040*Math.cos(Ob)))*(Math.pow((E/Nb),7))*(61+(662*t2b)+(1320*Math.pow(t2b,2))+(720*Math.pow(t2b,3))-(234*t2b*Math.pow(n2b,2))+(336*n2b*Math.pow(t2b,2))+(188*n2b)-(772*t2b*Math.pow(n2b,3))-(192*Math.pow(t2b,2)*Math.pow(n2b,2))+(88*Math.pow(n2b,5))-(2392*t2b*Math.pow(n2b,4))+(408*Math.pow(t2b,2)*Math.pow(n2b,3))+(1536*Math.pow(t2b,2)*Math.pow(n2b,4))-(1632*t2b*Math.pow(n2b,5))+(1920*Math.pow(t2b,2)*Math.pow(n2b,5)));
+    Longitud[4] = Longitud[0]-Longitud[1]+Longitud[2]-Longitud[3];
+    Longitud[5] = Longitud[4]/(Math.PI/180);
+    
+    let LatitudY:number = Latitud[6];
+    let LongitudX:number = Lo-Longitud[5];
+
+    let LatitudG = Math.trunc(LatitudY);
+    let LatitudM = Math.trunc((60*(LatitudY-LatitudG)));
+    let LatitudS = (LatitudY-LatitudG-LatitudM/60)*3600;
+
+    let LongitudG = Math.trunc(LongitudX);
+    let LongitudM = Math.trunc((60*(LongitudX-LongitudG)));
+    let LongitudS = (LongitudX-LongitudG-LongitudM/60)*3600;
+
+    // console.log(`Latitud => |  G:${this.LatitudG} M:${this.LatitudM} S:${this.LatitudS} | Decimal:${LatitudDecimal} Radianes:${LatitudRadians}`);
+    // console.log(`Longitud => |  G:${this.LongitudG} M:${this.LongitudM} S:${this.LongitudS} | Decimal:${LongitudDecimal} Radianes:${LongitudRadians}`);
+
+    console.log(
+      `DATUM => |  
+      a:${a} b:${b} c:${c}
+      e2:${e} e'2:${ee}
+      f:${f} n:${n}`
+    );
+
+    // console.log(
+    //   `VARIABLES => |  
+    //   N:${N} L:${L}
+    //   tt:${tt} nn:${nn}`
+    // );
+
+    this.resultados += `Conversión de Coordenadas UTM a Geodésicas (YYYY-MM-DD HH:MM:SS)\nLatitud: ${LatitudG} ${LatitudM} ${LatitudS}\nLongitud: ${LongitudG} ${LongitudM} ${LongitudS}\n`;
+
+    console.log(`LATITUD:${LatitudY}  LONGITUD:${LongitudX}`);
   }
 
   TME2UTM(){
@@ -139,7 +240,7 @@ export class CalculatorComponent implements OnInit {
     let f:number = ((a-b)/a);                                                           //Achatamiento del Elipsoide de Referencia
     let n:number = ((a-b)/(a+b));
 
-    //CALCULAR DISMath.tanCIA MERIDIONAL (Se calcula en base al DATUM)
+    //CALCULAR DISTANCIA MERIDIONAL (Se calcula en base al DATUM)
     let A0:number = 0.994977106051944;
     let A1:number = 0.005001851767455;
     let A2:number = 0.004184862497658;
@@ -147,7 +248,7 @@ export class CalculatorComponent implements OnInit {
     let A6:number = 0.000000123060249;
     let A8:number = 0.000000000744632;
 
-    //CONSMath.tanTES PARA CALCULO
+    //CONSTANTES PARA CALCULO
     let Ko:number = 0.9996;                                                             //Factor de Escala
     let No:number = 0;                                                                  //Falso Norte
     let Eo:number = 500000;                                                             //Falso Este
@@ -202,8 +303,8 @@ export class CalculatorComponent implements OnInit {
     let LatitudS = (LatitudY-LatitudG-LatitudM/60)*3600;
 
     let LongitudG = Math.trunc(LongitudX);
-    let LongitudM = Math.trunc((60*(LongitudX-LongitudG)));;
-    let LongitudS = (LongitudX-LongitudG-LongitudM/60)*3600;;
+    let LongitudM = Math.trunc((60*(LongitudX-LongitudG)));
+    let LongitudS = (LongitudX-LongitudG-LongitudM/60)*3600;
 
     // console.log(`Latitud => |  G:${this.LatitudG} M:${this.LatitudM} S:${this.LatitudS} | Decimal:${LatitudDecimal} Radianes:${LatitudRadians}`);
     // console.log(`Longitud => |  G:${this.LongitudG} M:${this.LongitudM} S:${this.LongitudS} | Decimal:${LongitudDecimal} Radianes:${LongitudRadians}`);
@@ -242,7 +343,7 @@ export class CalculatorComponent implements OnInit {
     let f:number = ((a-b)/a);                                                           //Achatamiento del Elipsoide de Referencia
     let n:number = ((a-b)/(a+b));
 
-    //CALCULAR DISMath.tanCIA MERIDIONAL (Se calcula en base al DATUM)
+    //CALCULAR DISTANCIA MERIDIONAL (Se calcula en base al DATUM)
     let A0:number = 0.994977106051944;
     let A1:number = 0.005001851767455;
     let A2:number = 0.004184862497658;
@@ -271,7 +372,7 @@ export class CalculatorComponent implements OnInit {
     this.MeridianoM = Number(z[1]);                                                       //Meridiano Minutos
     this.MeridianoS = Number(z[2]);                                                       //Meridiano Segundos
 
-    //CONSMath.tanTES PARA CALCULO
+    //CONSTANTES PARA CALCULO
     let Ko:number = 1;                                                             //Factor de Escala
     let No:number = 0;                                                                  //Falso Norte
     let Eo:number = 500000;                                                             //Falso Este
@@ -334,7 +435,7 @@ export class CalculatorComponent implements OnInit {
     let f:number = ((a-b)/a);                                                           //Achatamiento del Elipsoide de Referencia
     let n:number = ((a-b)/(a+b));
 
-    //CALCULAR DISMath.tanCIA MERIDIONAL (Se calcula en base al DATUM)
+    //CALCULAR DISTANCIA MERIDIONAL (Se calcula en base al DATUM)
     let A0:number = 0.994977106051944;
     let A1:number = 0.005001851767455;
     let A2:number = 0.004184862497658;
